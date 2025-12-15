88bd0c3eb5483e166aa406e33d5f4b4270628fab
HubertZodjihoue <joycetreavis@gmail.com>
2025-12-15 01:47:30 +0100

    fix java content

--- aws-serverless-upload/backend/src/main/java/com/serveless/Handler.java
+++ aws-serverless-upload/backend/src/main/java/com/serveless/Handler.java
@@ -18,7 +18,6 @@ public class Handler implements RequestHandler<Map<String, Object>, Map<String, 
         String filename = "test.txt";
         String key = "uploads/" + LocalDate.now() + "/" + filename;
         try (S3Presigner presigner = S3Presigner.create()) {
-
             PutObjectRequest putRequest = PutObjectRequest.builder()
                     .bucket(BUCKET)
                     .key(key)
